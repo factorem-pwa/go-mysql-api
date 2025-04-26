ca2696472b0e94c81501b8f0b2b13c545c32b735
juanpablo-agronorman <juanpablo@agronorman.com>
2025-04-26 12:18:13 -0600

    Actualización para usar el archivo .env para conectar con MySQL de forma segura

--- main.go
+++ main.go
@@ -2,62 +2,35 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
-// Product model (same as before)
-type Product struct {
-	gorm.Model
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-}
-
-var DB *gorm.DB
-
 func main() {
-	// Initialize Fiber
-	app := fiber.New()
-	app.Use(cors.New())
-	app.Use(logger.New())
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	// MySQL connection (replace credentials)
-	dsn := "user:password@tcp(db:3306)/products?charset=utf8mb4&parseTime=True&loc=Local"
+	// Get environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST") // Typically "db" for Docker or "localhost"
+
+	// Create DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbName)
+
+	// Connect to MySQL
 	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to connect to MySQL: " + err.Error())
+		log.Fatal("Failed to connect to database")
 	}
-	DB.AutoMigrate(&Product{})
-
-	// Routes (same as before)
-	app.Get("/api/products", getProducts)
-	app.Post("/api/products", createProduct)
-	app.Delete("/api/products/:id", deleteProduct)
-
-	// Start server
-	app.Listen(":3000")
-}
-
-// Handlers (unchanged)
-func getProducts(c *fiber.Ctx) error {
-	var products []Product
-	DB.Find(&products)
-	return c.JSON(products)
-}
-
-func createProduct(c *fiber.Ctx) error {
-	product := new(Product)
-	if err := c.BodyParser(product); err != nil {
-		return c.Status(400).SendString(err.Error())
-	}
-	DB.Create(&product)
-	return c.JSON(product)
-}
-
-func deleteProduct(c *fiber.Ctx) error {
-	id := c.Params("id")
-	DB.Delete(&Product{}, id)
-	return c.SendStatus(204)
+	
+	// ... rest of your code ...
 }
